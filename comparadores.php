e0a1541eac3196783429ac1dd084e59efcbfb6da
alvarosoriano2812@gmail.com <alvarosoriano2812@gmail.com>
2022-09-21 17:01:17 +0200

    fichero añadidos el 21 de sep a las 17:01

---
+++ comparadores.php
@@ -0,0 +1,17 @@
+<?php
+    $texto="0";
+    $numero =0;
+    if($texto==$numero){
+        echo "son iguales ya que se convierte el de tipo texto a entero para facilitar la conversion si pusieramos una cadena la
+        pondria en numeros enteros ";
+    }
+    else{
+        echo "son iguales y DEL MISMO";
+    }
+    if($texto===$numero){
+        echo "son iguales y del mismo tipo";
+    }
+    else{
+        echo "no son iguales ni del mismo tipo";
+    }
+?>
